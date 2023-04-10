cb0f871b5dd4053d59a3c1c951381a724954fb67
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-04-10 20:43:23 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -11,27 +11,24 @@ const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const routes = require('./routes');
 const path = require('path');
-const helmet = require('helmet');
 const csrf = require('csurf');
 const {middlewareGlobal, checkCsrfError,csrfMiddleware} = require('./src/middlewares/middleware');
 
-app.use(helmet());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
-    secret: 'Mensagem Flash',
+    secret: 'hgfdfgh bvbnbfg',
     store: MongoStore.create({mongoUrl: process.env.conectionString}),
     resave: false,
     saveUninitialized: false,
     cookie:{
-        maxAge: 1000*60*60*24*7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
         httpOnly: true
     }
 });
 app.use(sessionOptions);
-
 app.use(flash());
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
